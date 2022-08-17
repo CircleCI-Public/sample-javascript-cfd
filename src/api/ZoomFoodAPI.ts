419e7aa4d2fd8ff77e1717cd05274259e8b8e494
Jaryt Bustard <jarytbustard@gmail.com>
2022-08-17 02:29:26 -0400

    refactor: migrate to vue3 and fix tests

--- src/api/ZoomFoodAPI.ts
+++ src/api/ZoomFoodAPI.ts
@@ -1,30 +1,28 @@
-import axios, {AxiosResponse} from 'axios'
-import { MenuItem } from "../interfaces"
+import axios, { AxiosResponse } from "axios";
+import { MenuItem } from "../interfaces";
 
-const BASEURL = `${process.env.VUE_APP_API_URL}/${process.env.VUE_APP_NAME}/${process.env.VUE_APP_VERSION}`
+const BASEURL = `${process.env.VUE_APP_API_URL}/${process.env.VUE_APP_NAME}/${process.env.VUE_APP_VERSION}`;
 
 export default class ZoomFoodAPI {
-
-static async getMenu(): Promise<MenuItem[]> {
+  static async getMenu(): Promise<MenuItem[]> {
     const menuData = await axios.get<Promise<MenuItem[]>>(BASEURL + "/menu");
     return menuData.data;
-}
+  }
 
-static getImageUrl(imageID: number): string {
-    return `${BASEURL}/image/${imageID}`
-}
+  static getImageUrl(imageID: number): string {
+    return `${BASEURL}/image/${imageID}`;
+  }
 
-static async getCart(): Promise<MenuItem[]> {
+  static async getCart(): Promise<MenuItem[]> {
     const cartData = await axios.get<Promise<MenuItem[]>>(BASEURL + "/cart");
     return cartData.data;
-}
+  }
 
-static async addCartItem(item: MenuItem): Promise<AxiosResponse> {
+  static async addCartItem(item: MenuItem): Promise<AxiosResponse> {
     return axios.post(`${BASEURL}/cart`, item);
-}
+  }
 
-static async removeCartItem(item: MenuItem): Promise<AxiosResponse> {
+  static async removeCartItem(item: MenuItem): Promise<AxiosResponse> {
     return axios.delete(`${BASEURL}/cart/${item.id}`);
-}
-
+  }
 }
---
+++ tests/unit/menuCard.spec.ts
@@ -0,0 +1,19 @@
+import { mount } from "@vue/test-utils";
+import MenuItemCard from "@/components/MenuItemCard.vue";
+
+describe("CartItemCard.vue", () => {
+  it("renders the menuItem prop data", () => {
+    const menuItem = {
+      id: 1,
+      name: "French Fries",
+      description: "Fried potato rectangles",
+      price: "4.99",
+      imageId: "1",
+    };
+    const wrapper = mount(MenuItemCard, {
+      props: { menuItem },
+    } as any);
+
+    expect(wrapper.find("ion-card-title").text()).toEqual("French Fries");
+  });
+});
